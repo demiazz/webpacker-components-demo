9b58971050d7a359dbbab15df554ec2e43f16efe
Alexey Plutalov <demiazz.py@gmail.com>
2017-09-30 16:26:35 +0300

    Add `client/cable` for creating channel subscriptions

---
+++ frontend/client/cable.js
@@ -0,0 +1,42 @@
+import cable from "actioncable";
+
+function createCableUrl() {
+  const { protocol, hostname, port } = window.location;
+
+  const cableProtocol = protocol === "https:" ? "wss:" : "ws:";
+  const cableHost = hostname;
+  const cablePort = port ? `:${port}` : "";
+
+  const meta = document.querySelector("meta[name=action-cable-url");
+  const cablePath = meta.getAttribute("content");
+
+  return `${cableProtocol}//${cableHost}${cablePort}${cablePath}`;
+}
+
+function createCableConsumer() {
+  const url = createCableUrl();
+
+  return cable.createConsumer(url);
+}
+
+let consumer;
+
+function getCableConsumer() {
+  if (!consumer) {
+    if (process.env.NODE_ENV === "development") {
+      cable.startDebugging();
+    }
+
+    consumer = createCableConsumer();
+  }
+
+  return consumer;
+}
+
+function createChannel() {
+  const { subscriptions } = getCableConsumer();
+
+  return subscriptions.create.apply(subscriptions, arguments);
+}
+
+export default createChannel;
