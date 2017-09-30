97f7dd765ba1c20bffd5f48c91e8e7a58b477c4e
Alexey Plutalov <demiazz.py@gmail.com>
2017-09-30 20:25:10 +0300

    Subscribe on messages and render to messages list

--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,3 +1,13 @@
+import { subscribe } from "client/chat";
+
+import component from "utils/component";
+
 import "components/message/message";
 
 import "./messages.css";
+
+component(".messages", element => {
+  subscribe(message => {
+    element.insertAdjacentHTML("beforeend", message);
+  });
+});
